51ca16847ad96282c7a06cca07b304fb783d9404
Colten <likeyou600@gmail.com>
2023-02-13 23:44:57 +0800

    Feat : 添加CPE星等

--- Uva.py
+++ Uva.py
@@ -4,8 +4,10 @@ import sqlite3
 
 url_1 = 'https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1'
 url_2 = 'https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=2'
+url_3 = 'https://par.cse.nsysu.edu.tw/~advprog/star.php'
 html_1 = requests.get(url_1)
 html_2 = requests.get(url_2)
+html_3 = requests.get(url_3)
 
 sp1 = BeautifulSoup(html_1.text, 'html.parser')
 data1 = sp1.select("#col3_content_wrapper")
@@ -39,11 +41,20 @@ for site in sites:
             titles.append(tr.find_all('td')[2].find('a').string)
         key += 1
 
-
+stars = {}
+sp3 = BeautifulSoup(html_3.text, 'html.parser')
+data1 = sp3.select("#list0")
+trs = data1[0].find_all('tr')
+key = 0
+for tr in trs:
+    if key != 0:
+        stars.update(
+            {tr.find_all('td')[0].string: tr.find_all('td')[1].string})
+    key += 1
 conn = sqlite3.connect('Uva.sqlite')
 cursor = conn.cursor()
 
-sqlstr = 'CREATE TABLE IF NOT EXISTS codetopic("id"INTEGER PRIMARY KEY NOT NULL,"serial" INTEGER NOT NULL ,"title" TEXT NOT NULL,"topic" TEXT NOT NULL)'
+sqlstr = 'CREATE TABLE IF NOT EXISTS uva_topics("id"INTEGER PRIMARY KEY NOT NULL,"serial" INTEGER NOT NULL ,"title" TEXT NOT NULL,"topic_url" TEXT NOT NULL,"star" INTEGER)'
 cursor.execute(sqlstr)
 
 for title in titles:
@@ -59,8 +70,12 @@ for title in titles:
 
     website = "https://onlinejudge.org/external/" + \
         serial[:substring]+"/"+a[0]+".pdf"
+    try:
+        star = stars[str(a[0])]
+    except KeyError:
+        star = None
     cursor.execute(
-        "insert into codetopic (serial,title,topic) values (?,?,?)", (a[0], a[1], website))
+        "insert into uva_topics (serial,title,topic_url,star) values (?,?,?,?)", (a[0], a[1], website, star))
 
 conn.commit()
 conn.close()
